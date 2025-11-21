4b1ade3a27e48f1c46bfa0897dfe78161ca62c8c
AragonSnDev <aragonsndev@gmail.com>
2025-11-21 00:40:55 -0600

    Se agregan las capas de servicio y DAO de usuarios

---
+++ src/main/java/com/aragonsndev/api_users/users/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.aragonsndev.api_users.users.repository;
+
+import com.aragonsndev.api_users.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    boolean existsByEmail(String email);
+}
